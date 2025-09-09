b52dfa2729c63d3dbe261d0d6d7bc1a99a40913c
Jake Soenneker <jake@soenneker.com>
2025-09-09 12:15:24 -0500

    Reduces deps

--- test/Soenneker.Utils.AtomicBool.Tests/AtomicBoolTests.cs
+++ test/Soenneker.Utils.AtomicBool.Tests/AtomicBoolTests.cs
@@ -1,14 +1,9 @@
-﻿using Soenneker.Tests.FixturedUnit;
-using Xunit;
+﻿using Xunit;
 
 namespace Soenneker.Utils.AtomicBool.Tests;
 
-[Collection("Collection")]
-public sealed class AtomicBoolTests : FixturedUnitTest
+public sealed class AtomicBoolTests 
 {
-    public AtomicBoolTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
-    {
-    }
 
     [Fact]
     public void Default()
